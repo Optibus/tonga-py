274f525c551280b1847a8a5540320f5a358ce20d
eitany <eitan@optibus.com>
2021-10-17 10:56:15 +0300

    Remove analytics reporting flow, not relevant for on demand fetching

--- tests/test_client.py
+++ tests/test_client.py
@@ -1,12 +1,8 @@
-from time import sleep
-
-import json
-
 import unittest
 
 import requests_mock
 
-from tonga import TongaClient, TongaClientOptions
+from tonga import TongaClient
 
 
 class TestClient(unittest.TestCase):
@@ -58,47 +54,6 @@ class TestClient(unittest.TestCase):
         flag_value = client.get("flag_name")
         self.assertTrue(flag_value)
         self.assertEqual(1, m.call_count)
-
-    @requests_mock.Mocker()
-    def test_send_analytics_on_close(self, m):
-        server_url = "http://server_url"
-        m.get('{}/flag_value/flag_name1?user=some+user1&some_attribute=2'.format(server_url), text="true")
-        m.get('{}/flag_value/flag_name2?user=some+user1&some_attribute=2'.format(server_url), text="false")
-        m.get('{}/flag_value/flag_name3?user=some+user1&some_attribute=2'.format(server_url), status_code=404)
-        post = m.post('{}/update_analytics?user=some+user1&some_attribute=2'.format(server_url), status_code=200)
-        client = TongaClient(server_url, context_attributes=dict(user='some user1', some_attribute=2))
-        client.get("flag_name1")
-        client.get("flag_name1")
-        client.get("flag_name2")
-        client.get("flag_name3")
-
-        client.close()
-        self.assertTrue(post.called)
-        self.assertDictEqual({'flag_name1': {"true": 2}, "flag_name2": {"false": 1}, "flag_name3": {"null": 1}},
-                             json.loads(post.last_request.json()))
-
-    def test_close_without_being_started(self):
-        server_url = "http://server_url"
-        TongaClient(server_url).close()
-
-    @requests_mock.Mocker()
-    def test_send_analytics_periodically_and_not_duplicated(self, m):
-        server_url = "http://server_url"
-        m.get('{}/flag_value/flag_name1?user=some+user1&some_attribute=2'.format(server_url), text="true")
-        post = m.post('{}/update_analytics?user=some+user1&some_attribute=2'.format(server_url), status_code=200)
-        client = TongaClient(server_url, context_attributes=dict(user='some user1', some_attribute=2),
-                             options=TongaClientOptions(analytics_report_interval=0.01))
-        client.get("flag_name1")
-        sleep(0.1)
-        self.assertEqual(1, post.call_count)
-        self.assertDictEqual({'flag_name1': {"true": 1}}, json.loads(post.last_request.json()))
-        client.get("flag_name1")
-        sleep(0.1)
-        self.assertEqual(2, post.call_count)
-        self.assertDictEqual({'flag_name1': {"true": 1}}, json.loads(post.last_request.json()))
-        client.close()
-        # We dont expect more calls as no new calls were made since last report
-        self.assertEqual(2, post.call_count)
 
 
 if __name__ == '__main__':
--- tonga/client.py
+++ tonga/client.py
@@ -1,30 +1,19 @@
-from threading import Lock, Thread, Event
-from collections import defaultdict, Counter
-import json
 import urllib
 import requests
 import six
 
 
 class TongaClient(object):
-    def __init__(self, server_url, context_attributes=None, options=None):
+    def __init__(self, server_url, context_attributes=None):
         """
         :param server_url: Server connection string
         :type server_url: str
         :param context_attributes: Optional context attributes to be passed on each query
         :type context_attributes: dict[str, object]
-        :param options: Optional parameters defining the behavior of the client
-        :type options: TongaClientOptions
         """
         self.server_url = server_url
         self.context_attributes = context_attributes or {}
-        self.options = options or TongaClientOptions()
         self._flag_cache = {}
-        self._analytics_counter = defaultdict(Counter)
-        self._analytics_lock = Lock()
-        self._stop_event = Event()
-        self._update_analytics_thread = None
-        self._started = False
 
     def get(self, flag):
         """
@@ -35,39 +24,7 @@ class TongaClient(object):
         :return: Flag value if defined, otherwise None
         :rtype: Any
         """
-        self._ensure_started()
-        value = self._get_flag_value_through_cache(flag)
-        with self._analytics_lock:
-            self._analytics_counter[flag][json.dumps(value)] += 1
-        return value
-
-    def close(self):
-        """
-        Closes the client and underlying resources and flush any missing pending analytics report
-        """
-        self._stop_event.set()
-
-        if not self._started:
-            return
-
-        try:
-            self._update_analytics_thread.join(self.options.timeout_on_close)
-        except RuntimeError:
-            # If thread was not started
-            pass
-
-    def _ensure_started(self):
-        """
-        We want to start underlying resources in lazy manner only if the client is actually being used, this method
-        will start the client if needed and if its already started it will do nothing
-        """
-        if self._started:
-            return
-
-        self._update_analytics_thread = Thread(target=self._update_analytics_loop, name="Tonga Analytics Thread")
-        self._update_analytics_thread.setDaemon(True)
-        self._update_analytics_thread.start()
-        self._started = True
+        return self._get_flag_value_through_cache(flag)
 
     def _get_flag_value_through_cache(self, flag):
         """
@@ -114,46 +71,3 @@ class TongaClient(object):
         if query_string:
             return u"?" + query_string
         return ""
-
-    def _update_server_analytics(self):
-        """
-        Sends an update of accumulated analytics to the server, if none is accumulated since last invocation
-        this method does nothing
-        """
-        update = False
-        with self._analytics_lock:
-            if self._analytics_counter:
-                update = True
-                request_string = u'{server_url}/update_analytics'.format(server_url=self.server_url)
-                request_string += self._build_query_string()
-                analytics_body = json.dumps(self._analytics_counter)
-                self._analytics_counter.clear()
-        # Update out side of lock not to block threads waiting of network request
-        if update:
-            requests.post(request_string, json=analytics_body)
-
-    def _update_analytics_loop(self):
-        """
-        Runs the analytics update loop in a dedicated thread
-        """
-        while not self._stop_event.is_set():
-            self._update_server_analytics()
-            self._stop_event.wait(self.options.analytics_report_interval)
-
-        # Flush any pending analytics
-        self._update_server_analytics()
-
-
-class TongaClientOptions(object):
-    """
-    Configuration class for different options of the Tonga Client behavior
-    """
-    def __init__(self, analytics_report_interval=5, timeout_on_close=5):
-        """
-        :param timeout_on_close: How much to wait for underlying resources to gracefully close (seconds)
-        :type timeout_on_close: float
-        :param analytics_report_interval: Analytics thread interval between each report to the server (seconds)
-        :type analytics_report_interval: float
-        """
-        self.analytics_report_interval = analytics_report_interval
-        self.timeout_on_close = timeout_on_close
